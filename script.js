ff1ac8f02bdaa74707f83eb4ba75d44f40c11919
aryansighkumaya <98814995+aryansighkumaya@users.noreply.github.com>
2024-12-19 16:30:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const images = [
+    { url: "https://picsum.photos/id/237/200/300" },
+    { url: "https://picsum.photos/id/238/200/300" },
+    { url: "https://picsum.photos/id/239/200/300" },
+  ];
+
+
+const output=document.getElementById('output')
+const btn=document.getElementById('download-images-button')
+
 btn.addEventListener("click", () => {
     const imagePromises = images.map(image => {
         return new Promise((resolve, reject) => {
